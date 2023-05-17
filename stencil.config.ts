dc593ecb7f01d1765fb4d8c2cb48b118228f0956
LorenaMGvan <lorena.mgvan@hotmail.com>
2023-05-17 00:39:52 -0600

    agregar configuracion de jest

--- stencil.config.ts
+++ stencil.config.ts
@@ -2,6 +2,11 @@ import { Config } from '@stencil/core';
 
 export const config: Config = {
   namespace: 'prueba-nesimo',
+  testing: {
+    transform: {
+      '^.+\\.(ts|tsx|jsx|js)$': '<rootDir>/node_modules/@stencil/core/testing/jest-preprocessor.js'
+    }
+  },
   outputTargets: [
     {
       type: 'dist',
